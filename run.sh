db19c29eead3eb2f8e22d26f9f7185f9187a409c
Simon Brenner <olsner@gmail.com>
2017-03-01 18:46:07 +0100

    Make protocol/host/port configurable (somewhat)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export LC_ALL=en_US.UTF-8
-exec socat -d -v EXEC:'sed -runf ./irc.sed' OPENSSL:chat.freenode.net:6697,crnl
+exec socat -d -v EXEC:'sed -runf ./irc.sed' ${protocol-OPENSSL}:${host-chat.freenode.net}:${port-6697},crnl
